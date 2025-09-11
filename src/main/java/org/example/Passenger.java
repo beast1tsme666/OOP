868da01e20f2941c039e860681369b1ed374ef69
Nikita Pronkin <cerfcerfcerf96@gmail.com>
2025-09-11 22:27:24 +0300

    1 lab, 8 variant

--- src/main/java/org/example/Passenger.java
+++ src/main/java/org/example/Passenger.java
@@ -1,5 +1,22 @@
 package org.example;
 
 public class Passenger {
+    private String name;
+    private String passportNumber;
+
+    public Passenger(String name, String passportNumber) {
+        this.name = name;
+        this.passportNumber = passportNumber;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+
+
 }
-0sd0fk3o42opdsa',;,;lm
