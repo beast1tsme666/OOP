70af488615779902337352b95c58c2af48816a28
PronkinNV <cerfcerfcerf96@gmail.com>
2025-09-11 10:14:58 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        ///
+        ////
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
