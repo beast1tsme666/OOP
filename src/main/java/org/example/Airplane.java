f8f9bc94dc8b3c37b71a91700624393e03a4373b
Nikita Pronkin <cerfcerfcerf96@gmail.com>
2025-09-25 10:11:57 +0300

    1 lab, 8 variant

---
+++ src/main/java/org/example/Airplane.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Airplane {
+    private String model;
+    private String tailNumber;
+    private int capacity;
+
+    public Airplane(String model, String tailNumber, int capacity){
+        this.model = model;
+        this.tailNumber = tailNumber;
+        this.capacity = capacity;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public String getTailNumber(){
+        return tailNumber;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+
+
+
+
+
+
+
+}
